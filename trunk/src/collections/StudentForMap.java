7610ec51a0c3f2dbf356e0f12fd63c2aabe7f6e1
rajlaxmaniitg@gmail.com <rajlaxmaniitg@gmail.com@e3b3d9ec-6d73-f4cb-9e0b-e1a3946a666d>
2013-12-26 00:22:23 +0000

    Hash Map examples

---
+++ trunk/src/collections/StudentForMap.java
@@ -0,0 +1,97 @@
+package collections;
+
+//public class StudentForMap implements Comparable<StudentForMap> {
+
+	public class StudentForMap {
+	private int sno;
+	
+	private String name;
+	
+	private int age;
+	
+	
+	public String toString(){
+		return "Sno::"+this.sno+", Name::"+this.name+", Age::"+this.age+"\n";
+	}
+	
+	/*
+	public int hashCode(){
+		return sno+age+this.name.hashCode();
+	}
+	
+	public boolean equals(Object obj) {
+		
+		boolean res= false;
+		if (obj instanceof StudentForMap) {			
+			StudentForMap student  = (StudentForMap)obj;
+				if( student.getAge()==this.age 
+						&& student.sno==this.sno &&
+						student.getName().equalsIgnoreCase(this.name)){
+					res=true;	
+				}	
+		}
+		return res;
+	}
+	
+
+	@Override
+	public int compareTo(StudentForMap std) {
+		
+		return this.sno-std.getSno();
+		//return this.age-std.age;
+		//return (this.name).compareTo(std.getName());
+	}
+	 */
+	public StudentForMap(){
+		
+	}
+	
+	public StudentForMap(int sno, String name, int age){
+		this.sno=sno;
+		this.name=name;
+		this.age=age;
+	}
+
+
+	/**
+	 * @return the sno
+	 */
+	public int getSno() {
+		return sno;
+	}
+
+	/**
+	 * @param sno the sno to set
+	 */
+	public void setSno(int sno) {
+		this.sno = sno;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
