c982a4582ce38dd39f1691c3df98b2adb6f7b08a
rajlaxmaniitg@gmail.com <rajlaxmaniitg@gmail.com@e3b3d9ec-6d73-f4cb-9e0b-e1a3946a666d>
2014-01-04 05:21:38 +0000

    Exceptions

---
+++ trunk/src/exceptions/Bike.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class Bike extends Vehicle {
+
+	public void drive() throws EngineProblem, FlatTyre{
+		
+	}
+}
---
+++ trunk/src/exceptions/Vehicle.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class Vehicle {
+	
+	public void drive() throws EngineProblem, FlatTyre{
+	}
+}
